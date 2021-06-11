f9b4ae10649d88d904d680cd68447eab1e86c00b
winniediana4 <deinawinnie84@gmail.com>
2021-06-11 14:37:43 +0300

    add parent and child class

---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+
+class config:
+ '''
+ General configuration parent class
+ '''
+
+ NEWS_API_BASE_URL = 'http://newsapi.org/v2/top-headlines?country=us&category=business&apikey={}'
+ NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+ SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+   '''
+   Production configuration child class
+   Args:
+   Config: The parent configuration class with General configuration settings
+   '''
+   pass
+
+   class DEvConfig(Config):
+     '''
+     Production configuration child class
+
+     Args:
+     config:The parent configuration child class with General configuration settings
+     '''
+     pass
+     class DevConfig(config):
+       '''
+       Developement configuration child class
+
+       Args:
+       config:The parent configuration class with General configuration class with General configuration settings
+       '''
+
+       DEBUG = True 
+       config_options = {
+         'developement':DevConfig
+       }
+    
+     'developement':ProdConfig  

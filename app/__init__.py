b6c3dfdb1fe46c231d74d4f4dea702f6484dd8f3
winniediana4 <deinawinnie84@gmail.com>
2021-06-11 10:13:38 +0300

    set configuration

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from config import DevConfig
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from app import main as main_blueprint
+from .requests import configure_request
+
 
 bootstrap = Bootstrap
 
@@ -20,4 +22,4 @@ def create_app(config_name, config_options):
 
  app.register_blueprint(main_blueprint)
 
-
+ configure_request(app)

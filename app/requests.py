40eba583fca478207e6f9a53d65254758799ef73
winniediana4 <deinawinnie84@gmail.com>
2021-06-11 13:44:34 +0300

    get news

--- app/requests.py
+++ app/requests.py
@@ -32,7 +32,7 @@ def configure__request(app):
         news_results_list = get_news_response['results']
         news_results = (news_results_list)
 
-      return news_results(news):
+      return news_results(news)
       '''
       Function that process the news result and transform them to a list of objects
 
@@ -41,3 +41,47 @@ def configure__request(app):
       Returns :
       news_results:A list of news objects
       '''
+      news_results = []
+      for news_items in news:
+        id = news_items.get('id')
+        title = news_items.get('original_title')
+        overview = news_items.get('overview')
+        poster = news_items.get('poster_path')
+        vote_average = news_items.get('vote_average')
+        vote_count = news_item.get('vote_count')
+
+        if poster:
+          news_object = News(id, title,overview, poster, vote_average, vote_count)
+          news_results.append(news_object)
+
+          return news_results
+
+        def get_news(id):
+          get_news_details_url = base_url.format(id, api_key)
+
+          with urllib.resquest.urlopen(get_news_details_url)  as url:  
+            news_details_data = url.read()
+            news_details_response = json.loads(news_details_data)
+
+            news_object = None
+            if news_details_response:
+              id = news_details_response.get('id')
+              title = news_details_response.get('original_title')
+              overview = news_details_response.get('overview')
+              poster = news_details_response.get('poster_path')
+              vote_count = news_details_response.get('vote_count')
+              news_object = News(id, title,overview,poster, poster, vote_average,vote_count)
+
+              return news_object 
+
+              def search_news(news_name):
+                search_news_url = "http://newsapi.org/v2/top_headlines?country=us&category=business&apikey={}&query={}'.format(api_key, news_name"
+                withurllib.request.urlopen(search_news_data)
+
+                search_news_results = None
+
+                if search_news_response['results']:
+                  search_news = search_news_response['results']
+                  search_news_results = process_results()
+
+                  return search_news_results

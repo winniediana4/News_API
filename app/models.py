d826a923525ad238fd768d6170cb60f0e88132e2
winniediana4 <deinawinnie84@gmail.com>
2021-06-11 10:33:30 +0300

    save reviews

---
+++ app/models.py
@@ -0,0 +1,24 @@
+class news:
+  '''
+  news classto define news objects
+  '''
+
+  def __init__(self, id, title, overview, poster, vote_average, vote_count):
+    self.id = id
+    self.title = title
+    self.overview = overview
+    self.poster = "https:/images.wsj.net/im-247417/social" + poster
+    self.vote_average = vote_average
+    self.vote_count = vote_count 
+
+    class Review:
+      all_reviews = []
+
+      def __init__(self, news_id, title,imageurl, review):
+        self.news_id = news_id
+        self.title = title
+        self.imageurl = imageurl
+        self.reviews = review
+
+      def save_review(self):
+          Review.all_reviews.append(self) 
